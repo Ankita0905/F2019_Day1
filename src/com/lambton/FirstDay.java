48109940f0923c50f5bd88205938ecd344787cde
Ankita Jain <ankita.jain12342@gmail.com>
2019-09-18 19:24:04 -0400

    [Ankita] Comment Added

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -5,10 +5,14 @@ public class FirstDay
     public static void main(String[] args)
     {
         System.out.println("Ankita Jain");
+        //Variable declaration
         int a, b, c;
+        //value Assignment
         a=10;
         b=20;
+        //Perform Add
                  c = a + b;
+                 //Submit result
         System.out.println("the sum is " +c);
     }
 }
